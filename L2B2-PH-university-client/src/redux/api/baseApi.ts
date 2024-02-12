3e7f3be14a04be58b825544123ee6adc0131445c
Kakon Debnath <kakondebnath96@gmail.com>
2024-02-12 19:44:40 +0600

    add token to headers of base query

--- L2B2-PH-university-client/src/redux/api/baseApi.ts
+++ L2B2-PH-university-client/src/redux/api/baseApi.ts
@@ -1,10 +1,18 @@
+import { RootState } from './../store';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+const baseQuery = fetchBaseQuery({
+  baseUrl: 'http://localhost:5000/api/v1',
+  credentials: 'include',
+  prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState).auth.token;
+    if (token) {
+      headers.set('Authorization', token);
+    }
+  },
+});
 
 export const baseApi = createApi({
   reducerPath: 'baseApi',
-  baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:5000/api/v1',
-    credentials: 'include',
-  }),
+  baseQuery: baseQuery,
   endpoints: () => ({}),
 });
